c7e8e2fccc052c1537fa61a5ff1b8c22cb6d1e7c
Hans Sebastian <hans@julofinance.com>
2019-11-02 15:18:57 +0700

    third commit

--- tests/test_pycon_2019_demo.py
+++ tests/test_pycon_2019_demo.py
@@ -1,4 +1,5 @@
 from pycon_2019_demo import __version__
+from pycon_2019_demo import models
 
 
 def test_version():
@@ -12,4 +13,10 @@ def test_version1():
 def test_version2():
     import time
     assert __version__ == '0.1.0'
-    time.sleep(11)
+    time.sleep(4)
+
+def test_models():
+    assert models.get_all()
+
+def test_none():
+    assert models.get_none()
